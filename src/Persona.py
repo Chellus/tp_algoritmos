ce3db442e1c9678b2af6590f05b621f30bc1cb38
Elias Riveros <elias4845@gmail.com>
2023-05-04 15:57:01 -0400

    Clase de persona creada

---
+++ src/Persona.py
@@ -0,0 +1,26 @@
+class Persona:
+    def __init__(self, cod, ci, apellido, nombre, telefono, direccion, dependencia, salario):
+        self.cod = cod
+        self.ci = ci
+        self.apellido = apellido
+        self.nombre = nombre
+        self.telefono = telefono
+        self.direccion = direccion
+        self.dependencia = dependencia
+        self.salario = salario
+
+    def IngresarPersona(self):
+        # TODO
+        pass
+
+    def EliminarPersona(self):
+        # TODO
+        pass
+
+    def ModificarPersona(self):
+        # TODO
+        pass
+
+    def AsignarDependencia(self):
+        # TODO
+        pass
