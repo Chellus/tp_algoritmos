17f4b222202a0a77ee861cd4b1e0c71e487a13cb
ferflex <fernanfleitas@gmail.com>
2023-05-05 15:00:38 -0400

    class dependencia

--- src/Dependencia.py
+++ src/Dependencia.py
@@ -1,6 +1,8 @@
-class dependencia()
+class dependencia:
     def __init__(self, COD, NOMBRE, CODRES):
-        self.COD = COD
-        self.NOMBRE = NOMBRE
-        self.CODRES = CODRES
+        self.COD = COD                          #Codigo único de Dependecia.
+        self.NOMBRE = NOMBRE                    #Nombre de la dependencia.
+        self.CODRES = CODRES                    #Código de persona del jefe de la dependencia.
+
+    #CREAR FUNCIONES DE LA CLASE DEPENDENCIA
         
