d4d1a4958794513d325d13d901f019ad35c1ec12
ferflex <fernanfleitas@gmail.com>
2023-05-24 17:25:49 -0400

    messsiiiii gool

--- code/fer_Ati/main.py
+++ code/fer_Ati/main.py
@@ -14,4 +14,13 @@ if __name__ == "__main__":
     window.show()
     sys.exit(app.exec_())
 
+ '''
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QStackedWidget, QVBoxLayout, QPushButton, QLabel, QFrame, QHBoxLayout, QVBoxLayout, QSpacerItem
+from PyQt5.QtWidgets import QApplication, QWidget, QSizePolicy
+from PyQt5.QtGui import QCursor, QIcon, QFont
+from PyQt5.uic import loadUi
+from PyQt5.QtCore import QCoreApplication, Qt, QSize
+from PyQt5.QtCore import QMetaObject
  
+ '''
